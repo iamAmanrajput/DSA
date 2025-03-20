12f0f65c0999d3ee3d8ebe0c21070b88c7ef969e
Aman Kumar <aman.it360@gmail.com>
2025-03-20 10:24:11 +0530

    Optimize version of sieve

--- 88/index.cpp
+++ 88/index.cpp
@@ -8,7 +8,8 @@ vector<bool> Sieve(int n)
 {
     vector<bool> sieve(n + 1, true);
     sieve[0] = sieve[1] = false;
-    for (int i = 2; i <= n; i++)
+    // optimization 2 --> outer loop
+    for (int i = 2; i * i <= n; i++)
     {
         if (sieve[i])
         {
