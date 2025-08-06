cea0ba7d093c6c09a79cc6ff2cc00bce0c158217
Aman Kumar <aman.it360@gmail.com>
2025-08-06 10:23:05 +0530

    feat: add implementation of doubly linked list

---
+++ 137/index.cpp
@@ -0,0 +1,70 @@
+// Doubly Linked List
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+    ~Node()
+    {
+        cout << "Node with data " << this->data << " deleted" << endl;
+    }
+};
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int getLength(Node *&head)
+{
+    int length = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        length++;
+    }
+    return length;
+}
+
+int main()
+{
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+
+    first->next = second;
+    second->prev = first;
+
+    second->next = third;
+    third->prev = second;
+
+    print(first); // 10 20 30
+
+    return 0;
+}
