d87a1cc414a69f4154307abea375d6e1dbff099d
Aman Kumar <aman.it360@gmail.com>
2025-08-08 10:15:22 +0530

    feat: add implementation to find the middle node of a linked list

---
+++ 138/mid.cpp
@@ -0,0 +1,94 @@
+// Find middle of Linked List
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+    ~Node()
+    {
+        cout << "Node with data " << this->data << " deleted" << endl;
+    }
+};
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node *getMiddle(Node *&head)
+{
+    if (head == NULL)
+    {
+        cout << "Linked list is Empty" << endl;
+        return head;
+    }
+
+    if (head->next == NULL)
+    {
+        return head;
+    }
+
+    Node *slow = head;
+    Node *fast = head->next; // even ke case me 30 ko hi answer consider karega 
+
+    while (slow != NULL && fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+    return slow;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+    Node *fourth = new Node(40);
+    Node *fifth = new Node(50);
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = NULL;
+
+    print(head);
+
+    cout << "Middle node is : " << getMiddle(head)->data << endl;
+
+    return 0;
+}
+
+// output
+// 10 20 30 40 50
+// Middle node is : 30
