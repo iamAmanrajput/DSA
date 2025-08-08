35d77276bdcdd4d14dfe7d9017b38be1f74cf943
Aman Kumar <aman.it360@gmail.com>
2025-08-08 13:20:16 +0530

    feat: 141. Linked List Cycle -- (LC) using Floyd’s algorithm

---
+++ 138/cycleDetection/index.cpp
@@ -0,0 +1,61 @@
+// 141. Linked List Cycle -- (LC)
+// # using Floyd’s Cycle Detection (Tortoise and Hare)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list node
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (!head || !head->next)
+            return false; // Empty or single node -> no cycle
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast && fast->next)
+        {
+            slow = slow->next;       // Move 1 step
+            fast = fast->next->next; // Move 2 steps
+
+            if (slow == fast)
+                return true; // Cycle detected
+        }
+
+        return false; // fast reached NULL => no cycle
+    }
+};
+
+// Example usage
+int main()
+{
+    // Create nodes
+    ListNode *head = new ListNode(3);
+    ListNode *second = new ListNode(2);
+    ListNode *third = new ListNode(0);
+    ListNode *fourth = new ListNode(-4);
+
+    // Link nodes: 3 -> 2 -> 0 -> -4 -> 2 (cycle)
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = second; // cycle here
+
+    Solution obj;
+    if (obj.hasCycle(head))
+        cout << "Cycle detected\n";
+    else
+        cout << "No cycle\n";
+
+    return 0;
+}
