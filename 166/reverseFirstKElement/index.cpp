2d191ea3734df257a290bcbef016283e45b70122
Aman Kumar <aman.it360@gmail.com>
2025-12-22 20:15:56 +0530

    feat: implement solution of Gfg Problem - Reverse k elements of queue using recursion

---
+++ 166/reverseFirstKElement/index.cpp
@@ -0,0 +1,87 @@
+// Gfg Problem - Reverse k elements of queue
+// Tc - 0(N)
+// Sc - 0(1)
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+    // Helper function to reverse first k elements using recursion
+    void helper(queue<int> &q, int k)
+    {
+        // Base case
+        if (k == 0)
+        {
+            return;
+        }
+
+        // Step 1: Front element nikaalo
+        int temp = q.front();
+        q.pop();
+
+        // Step 2: Recursive call
+        helper(q, k - 1);
+
+        // Step 3: Backtracking ke time element ko queue ke end me push karo
+        q.push(temp);
+    }
+
+    // Function to reverse first k elements of the queue
+    queue<int> reverseFirstK(queue<int> q, int k)
+    {
+
+        // Edge case check
+        if (k <= 0 || k > q.size())
+            return q;
+
+        // Reverse first k elements
+        helper(q, k);
+
+        // Remaining elements ko rotate karke order maintain karo
+        int remainingElem = q.size() - k;
+        while (remainingElem--)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+
+        return q;
+    }
+};
+
+int main()
+{
+    queue<int> q;
+
+    // Test Case
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    int k = 3;
+
+    Solution obj;
+    queue<int> result = obj.reverseFirstK(q, k);
+
+    // Output print karo
+    cout << "Queue after reversing first " << k << " elements: ";
+    while (!result.empty())
+    {
+        cout << result.front() << " ";
+        result.pop();
+    }
+
+    return 0;
+}
+
+// Input
+// Queue = 1 2 3 4 5
+// k = 3
+
+// Output
+// Queue after reversing first 3 elements: 3 2 1 4 5
