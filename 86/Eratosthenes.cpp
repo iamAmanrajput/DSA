d30495dcbd15d45336776101998453cd6740d3ae
Aman Kumar <aman.it360@gmail.com>
2026-05-19 17:01:22 +0530

    feat: optimize Sieve of Eratosthenes implementation for counting primes

--- 86/Eratosthenes.cpp
+++ 86/Eratosthenes.cpp
@@ -1,6 +1,3 @@
-// finding prime number using Sieve of Eratosthenes
-// # Sieve of Eratosthenes TC = O(n log log n)
-
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -10,27 +7,29 @@ class Solution
 public:
     int countPrimes(int n)
     {
-        if (n == 0)
+        if (n <= 1)
             return 0;
 
-        vector<bool> prime(n, true);
+        vector<bool> prime(n + 1, true);
         prime[0] = prime[1] = false;
 
         int ans = 0;
 
-        for (int i = 2; i < n; i++)
+        for (int i = 2; i <= n; i++)
         {
             if (prime[i])
             {
                 ans++;
+
                 int j = 2 * i;
-                while (j < n)
+                while (j <= n)
                 {
                     prime[j] = false;
-                    j = j + i;
+                    j += i;
                 }
             }
         }
+
         return ans;
     }
 };
